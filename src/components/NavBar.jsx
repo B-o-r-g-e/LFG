c2b50ff4f2b5c0ab4d4819a7cf9518586fd75cfa
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-07-14 07:34:59 +0100

    added dextool

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,22 @@
 import { FaTwitter } from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
 import dex from "../assets/dex.svg";
+import dextool from "../assets/dextool.png";
 
 
 const NavBar = () => {
     return (
         <div className={'w-full py-10 flex items-center justify-between'}>
             <h1 className={'font-black text-2xl'}>LFG</h1>
-            <div className={'flex gap-4'}>
+            <div className={'flex'}>
                 <ul className={'flex items-center gap-4'}>
                     <li className={'text-3xl'} ><a href="https://x.com/ScaredMoneycto"><FaTwitter /></a> </li>
                     <li className={'text-3xl'}><a href="https://t.me/SCAREDMONEYLFGSOL"><FaTelegram /></a></li>
                     <li className={'p-2 rounded-full bg-gray-900'}><a href="https://dexscreener.com/solana/6RfBTrFyttmGuRCdRgx1veeqP2g5wFB6cG9mUuiKkejS">
                         <img className={"w-6 h-6"} src={dex} alt="dex-image"/>
+                    </a></li>
+                    <li className={'p-2 rounded-full bg-gray-900'}><a href="https://www.dextools.io/app/en/solana/pair-explorer/6RfBTrFyttmGuRCdRgx1veeqP2g5wFB6cG9mUuiKkejS?t=1720912494004">
+                        <img className={"w-6 h-6"} src={dextool} alt="dex-image"/>
                     </a></li>
                 </ul>
             </div>
